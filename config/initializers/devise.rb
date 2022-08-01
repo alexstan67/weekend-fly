2d0fff4b32fc0dc9ab4380fb0eeedd44a480655a
Alexandre Stanescot <alex@aerostan.com>
2022-08-01 16:34:04 +0200

    email sender update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'alex@aerostan.com'
+  config.mailer_sender = 'noreply@aerostan.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
