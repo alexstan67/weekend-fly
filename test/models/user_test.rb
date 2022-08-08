8f18d1d3bec93ace66734ce93759c04fe902298c
Alexandre Stanescot <alex@aerostan.com>
2022-08-08 10:48:32 +0200

    Test updates after homebase removal

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,22 +6,15 @@ class UserTest < ActiveSupport::TestCase
   # end
   
   test "Correct user should save" do
-    assert User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123456", homebase: "ELLX").save
+    assert User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123456").save
   end
  
   test "email validation should trigger" do
-    assert_not User.new(password: "123456", first_name: "First name", last_name: "Last Name", homebase: "ELLX").save
+    assert_not User.new(password: "123456", first_name: "First name", last_name: "Last Name").save
   end
 
-  test "homebase validation should trigger" do
-    assert_not User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123456").save
-  end
-  
   test "Too short password should not save" do
-    assert_not User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123", homebase: "ELLX").save
+    assert_not User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123").save
   end
 
-  test "Wrong ICAO should not save" do
-    assert_not User.new(email: "test@test.com", first_name: "First name", last_name: "Last Name", password: "123456", homebase: "LF5755").save
-  end
 end
