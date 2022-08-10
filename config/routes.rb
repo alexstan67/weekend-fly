6091d35afe85ec7bc8baa6b346ef533b5837f90b
Alexandre Stanescot <alex@aerostan.com>
2022-08-10 10:48:35 +0200

    model and routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/trip_input', to: 'trip_input#new'
+  post '/trip_input', to: 'trip_input#create'
 
   # Defines the root path route ("/")
   root to: "pages#home"
