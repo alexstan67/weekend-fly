d48155bf91fc7fadcb043567b755845cfd77fe59
Alexandre Stanescot <alex@aerostan.com>
2022-08-24 12:05:40 +0200

    Edit button for trip_inputs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  resources :trip_inputs, only: [:new, :create, :edit, :update] 
   get 'trip_outputs/index'
-  get 'trip_inputs/new'
-  get 'trip_inputs/create'
-  post 'trip_inputs/create'
   get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- test/controllers/trip_inputs_controller_test.rb
+++ test/controllers/trip_inputs_controller_test.rb
@@ -6,23 +6,23 @@ class TripInputsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not get new until logged in" do
-    get trip_inputs_new_url
+    get new_trip_input_url
     assert_redirected_to new_user_session_path
   end
 
   test "should get new" do
     login_as users(:robert)
-    get trip_inputs_new_url
+    get new_trip_input_url
     assert_response :success
   end
   
   test "should create a new trip_input" do
     login_as users(:robert)
-    get trip_inputs_new_url
+    get new_trip_input_url
     assert_response :success
 
     assert_difference('TripInput.count') do
-      post trip_inputs_create_path, params: { trip_input: { user_id: @user.id, dep_airport_icao: "ELLX", dep_in_hour: 2, eet_hour: 2, distance_nm: 100, overnights: 2, flight_back: "AM", small_airport: true, medium_airport: true, large_airport: true, international_flight: false}}
+      post trip_inputs_path, params: { trip_input: { user_id: @user.id, dep_airport_icao: "ELLX", dep_in_hour: 2, eet_hour: 2, distance_nm: 100, overnights: 2, flight_back: "AM", small_airport: true, medium_airport: true, large_airport: true, international_flight: false}}
     end
 
     assert_redirected_to trip_outputs_index_url
@@ -30,11 +30,11 @@ class TripInputsControllerTest < ActionDispatch::IntegrationTest
   
   test "should not create a new trip_input" do
     login_as users(:robert)
-    get trip_inputs_new_url
+    get new_trip_input_url
     assert_response :success
 
     assert_no_difference('TripInput.count') do
-      post trip_inputs_create_path, params: { trip_input: { user_id: @user.id, dep_airport_icao: "ZZZZ", dep_in_hour: 2, eet_hour: 2, distance_nm: 100, overnights: 2, flight_back: "AM", small_airport: true, medium_airport: true, large_airport: false, international_flight: true}}
+      post trip_inputs_path, params: { trip_input: { user_id: @user.id, dep_airport_icao: "ZZZZ", dep_in_hour: 2, eet_hour: 2, distance_nm: 100, overnights: 2, flight_back: "AM", small_airport: true, medium_airport: true, large_airport: false, international_flight: true}}
     end
 
   end
