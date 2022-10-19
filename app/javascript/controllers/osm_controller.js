89b75c4f2204a17c01eccbdc627f790c4be21ffd
Alexandre Stanescot <alex@aerostan.com>
2022-10-19 19:24:10 +0200

    Changed osm tile to standard map yet

--- app/javascript/controllers/osm_controller.js
+++ app/javascript/controllers/osm_controller.js
@@ -22,15 +22,16 @@ export default class extends Controller {
     this.map = L.map(this.mapTarget).setView([this.centerValue[0].lat, this.centerValue[0].lon], 6);
     
     // We display the tile layer
-    /*
     // Standard map
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(this.map);
-    */
+
     // Topographic Map
+    /*
     L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 17, attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}).addTo(this.map);
+    */
 
     // We display the homebase marker
     var marker = L.marker([this.centerValue[0].lat, this.centerValue[0].lon]).addTo(this.map);
