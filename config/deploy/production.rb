b097cd97a89c226bc6f014db8d07afc9346e9b76
Alexandre Stanescot <alex@aerostan.com>
2023-03-03 15:33:18 +0100

    Deployment config update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '54.37.152.178', user: 'deploy', roles: %w{app db web}
+server '192.168.77.19', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
