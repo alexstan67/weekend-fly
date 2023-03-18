310b67a7523b1405f02ca5133d9d74b41299fca8
Alexandre Stanescot <alex@aerostan.com>
2023-03-18 19:20:42 +0100

    Fix: production server url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = { host: 'http://www.weekend-fly.com', port: 8080 }
+  config.action_mailer.default_url_options = { host: 'fly.aerostan.com', port: 8080 }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
