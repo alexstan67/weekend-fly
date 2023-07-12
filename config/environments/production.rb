5637bab8bdde9c566140e0e8b919993f80b879e2
Alexandre Stanescot <alex@aerostan.com>
2023-07-12 10:17:20 +0200

    Project renamed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "weekend_fly_production"
+  # config.active_job.queue_name_prefix = "wef_2022_production"
 
   config.action_mailer.perform_caching = false
 
